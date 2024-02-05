80cc42c7dd5765e4b24998219cd90ea08567f595
yibudak <yibudak@gmail.com>
2024-02-05 12:33:33 +0300

    [FIX] Update glossaries on write and unlink

--- web_translate_deepl/models/deepl_glossary.py
+++ web_translate_deepl/models/deepl_glossary.py
@@ -1,6 +1,11 @@
 # Copyright 2024 Ahmet Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import csv
+import io
+import base64
+import requests
 
 
 class DeepLGlossary(models.Model):
@@ -35,9 +40,67 @@ class DeepLGlossary(models.Model):
         """
         if "deepl_id" not in vals:
             vals["deepl_id"] = False
+            # Delete the glossary from DeepL API too.
+            for glossary in self.filtered(lambda g: g.deepl_id):
+                glossary._api_delete_glossary(glossary.deepl_account_id)
+
         return super(DeepLGlossary, self).write(vals)
+
+    def unlink(self):
+        """
+        Delete the glossary from DeepL API before unlinking.
+        :return:
+        """
+        for glossary in self.filtered(lambda g: g.deepl_id):
+            glossary._api_delete_glossary(glossary.deepl_account_id)
+        return super(DeepLGlossary, self).unlink()
 
     @api.depends("deepl_id", "source_lang_id", "target_lang_id")
     def _compute_name(self):
         for record in self:
             record.name = f"[{record.deepl_account_id.name}] {record.source_lang_id.name} > {record.target_lang_id.name}"
+
+    def _api_delete_glossary(self, account):
+        """
+        Delete glossary from DeepL API.
+        :param account: deepl.account record
+        :return:
+        """
+        self.ensure_one()
+        url = f"https://api.deepl.com/v2/glossaries/{self.deepl_id}"
+        headers = {
+            "Authorization": f"DeepL-Auth-Key {account.auth_key}",
+            "User-Agent": "Odoo/12.0",
+        }
+        requests.delete(url, headers=headers, timeout=10)
+        self.write({"deepl_id": False})
+
+    def _api_create_glossary(self, account):
+        """
+        Create glossary in DeepL API.
+        :param account: deepl.account record
+        :return:
+        """
+        self.ensure_one()
+        csv_file = io.StringIO(base64.b64decode(self.entries_csv_file).decode("utf-8"))
+        reader = csv.reader(csv_file)
+        reader.__next__()  # Skip header
+        entries = list(reader)
+        entries_text = "\n".join("%s\t%s" % (x[0], x[1]) for x in entries)
+
+        url = "https://api.deepl.com/v2/glossaries"
+        headers = {
+            "Authorization": f"DeepL-Auth-Key {account.auth_key}",
+            "User-Agent": "Odoo/12.0",
+        }
+        data = {
+            "name": self.name,
+            "source_lang": self.source_lang_id.code.split("_")[0],
+            "target_lang": self.target_lang_id.code.split("_")[0],
+            "entries": entries_text,
+            "entries_format": "tsv",
+        }
+        response = requests.post(url, json=data, headers=headers, timeout=10)
+        if response.status_code != 201:
+            raise UserError(_("DeepL API Error: %s") % response.text)
+        self.write({"deepl_id": response.json().get("glossary_id")})
